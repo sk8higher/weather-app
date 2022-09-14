adf9b62ca2d334e67226e06f5e0aeb8d17a7bf73
sk8higher <sk8higher@iballwasrawt.ru>
2022-09-14 20:13:47 +0300

    Initial comms

--- src/index.js
+++ src/index.js
@@ -2,11 +2,15 @@ import fetchWeather from './datafetch/fetch-weather';
 import getUserLocation from './datafetch/get-geolocation';
 
 async function drawUI() {
+  // Fetching user location from browser ang getting coords
   const { latitude: lat, longitude: lng } = await getUserLocation().then(
     (coords) => coords.coords
   );
 
+  // Building a link for api request
   const link = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lng}&appid=66a86bcca603db7ee72a9999499bb3fa&units=metric`;
+
+  // Fetching the weather and appending info to the page
   fetchWeather(link).then((resp) => {
     console.log(resp);
     document.body.insertAdjacentHTML('beforeend', 'it is  ');
