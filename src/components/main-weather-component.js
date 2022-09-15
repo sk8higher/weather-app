9b24cb664d73ccc08e5a53a0dbff6edf19155b93
sk8higher <sk8higher@iballwasrawt.ru>
2022-09-15 23:38:40 +0300

    Add styles to main weather components

--- src/components/main-weather-component.js
+++ src/components/main-weather-component.js
@@ -1,7 +1,18 @@
 function mainWeatherComponent(weatherInfo) {
-  return `<p>${Number(weatherInfo.main.temp).toFixed(0)} C in ${
-    weatherInfo.name
-  }</p>`;
+  const deg = Number(weatherInfo.main.temp).toFixed(0);
+  const icon = weatherInfo.weather[0].icon;
+  const description = weatherInfo.weather[0].main;
+
+  return `
+  <div class="main-weather-container">
+  <div class="main-weather__info-container">
+    <p class="weather-info">${description}</p>
+  </div>
+    <div class="main-weather__temp-container">
+      <img class="weather-icon" src="http://openweathermap.org/img/wn/${icon}.png"/>
+      <p class="main-weather-degrees">${deg}°C</p>
+    </div>
+  </div>`;
 }
 
 export default mainWeatherComponent;
