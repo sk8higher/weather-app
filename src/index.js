7ed41d22ec67291d168774722cdb11bafda5c1cc
sk8higher <sk8higher@iballwasrawt.ru>
2022-09-17 17:36:42 +0300

    Error handling if the user's location is disabled

--- src/index.js
+++ src/index.js
@@ -1,15 +1,22 @@
 import getUserLocation from './datafetch/get-geolocation';
-
 import renderUI from './components/renderUI';
+
 import './styles.css';
 
 const wrapper = document.querySelector('.wrapper');
 
 async function init() {
   // Fetching user location from browser ang getting coords
-  const { latitude: lat, longitude: lng } = await getUserLocation().then(
-    (coords) => coords.coords
-  );
+  const { latitude: lat, longitude: lng } = await getUserLocation()
+    .then((coords) => coords.coords)
+    .catch((error) => {
+      if (error.code === 1) {
+        wrapper.insertAdjacentHTML(
+          'beforeend',
+          '<p>Please enable location and restart page</p>'
+        );
+      }
+    });
 
   // Building a link for api request
   const link = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lng}&appid=66a86bcca603db7ee72a9999499bb3fa&units=metric`;
