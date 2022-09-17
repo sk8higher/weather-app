9cff668ac67e7afb8bfdcb598ece0963b75c8eb0
sk8higher <sk8higher@iballwasrawt.ru>
2022-09-17 13:52:38 +0300

    Add search bar

---
+++ src/components/searchbar-component.js
@@ -0,0 +1,10 @@
+function searchBarComponent() {
+  return `
+  <div class="search-form-container">
+    <form class="search-form">
+      <input id="searchbar" type="text" class="input" placeholder="search for a city"/>
+    </form>
+  </div>`;
+}
+
+export default searchBarComponent;
