c6149d7bcac3aa9cd171e809fd4368724d8399f6
sk8higher <sk8higher@iballwasrawt.ru>
2022-09-17 15:54:29 +0300

    Refactor UI render

--- src/index.js
+++ src/index.js
@@ -14,7 +14,24 @@ async function init() {
   // Building a link for api request
   const link = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lng}&appid=66a86bcca603db7ee72a9999499bb3fa&units=metric`;
 
-  await renderUI(wrapper, link);
+  renderUI(wrapper, link);
 }
 
 init();
+
+document.body.addEventListener('submit', function (e) {
+  e.preventDefault();
+
+  const formInput = document.querySelector('#searchbar');
+  const errorContent = document.querySelector('p .error');
+
+  const cityname = formInput.value;
+  const reverseGeoLink = `https://api.openweathermap.org/data/2.5/weather?q=${cityname}&appid=66a86bcca603db7ee72a9999499bb3fa&units=metric`;
+
+  try {
+    renderUI(wrapper, reverseGeoLink);
+  } catch (error) {
+    errorContent.classList.add('error');
+    errorContent.text = error;
+  }
+});
