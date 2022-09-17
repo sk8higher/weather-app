31d5f20a804a3ecd6d15ca977a8574a05b5af7fb
sk8higher <sk8higher@iballwasrawt.ru>
2022-09-17 15:10:53 +0300

    Refactor init page function, create renderUI function

---
+++ src/components/renderUI.js
@@ -0,0 +1,26 @@
+import searchBarComponent from './searchbar-component';
+import locationComponent from './location-component';
+import mainWeatherComponent from './main-weather-component';
+import extraInfoComponent from './extra-info-component';
+import fetchWeather from '../datafetch/fetch-weather';
+
+import '../styles.css';
+
+/**
+ * Renders all app components. Uses an element, where it will be inserting HTML and link to fetch weather
+ * @param {HTMLElement} element
+ * @param {string} link
+ */
+async function renderUI(element, link) {
+  fetchWeather(link).then((resp) => {
+    element.innerHTML = '';
+    element.insertAdjacentHTML('beforeend', searchBarComponent());
+    element.insertAdjacentHTML('beforeend', locationComponent(resp));
+    element.insertAdjacentHTML('beforeend', mainWeatherComponent(resp));
+
+    const mainWeatherEl = document.querySelector('.main-weather-container');
+    mainWeatherEl.insertAdjacentHTML('beforeend', extraInfoComponent(resp));
+  });
+}
+
+export default renderUI;
--- src/index.js
+++ src/index.js
@@ -1,15 +1,11 @@
-import fetchWeather from './datafetch/fetch-weather';
 import getUserLocation from './datafetch/get-geolocation';
-import mainWeatherComponent from './components/main-weather-component';
-import locationComponent from './components/location-component';
-import extraInfoComponent from './components/extra-info-component';
-import searchBarComponent from './components/searchbar-component';
 
+import renderUI from './components/renderUI';
 import './styles.css';
 
 const wrapper = document.querySelector('.wrapper');
 
-async function drawUI() {
+async function init() {
   // Fetching user location from browser ang getting coords
   const { latitude: lat, longitude: lng } = await getUserLocation().then(
     (coords) => coords.coords
@@ -18,37 +14,7 @@ async function drawUI() {
   // Building a link for api request
   const link = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lng}&appid=66a86bcca603db7ee72a9999499bb3fa&units=metric`;
 
-  // Fetching the weather and appending info to the page
-  fetchWeather(link).then((resp) => {
-    console.log(resp);
-    wrapper.insertAdjacentHTML('beforeend', searchBarComponent());
-
-    const form = document.querySelector('.search-form');
-    form.addEventListener('submit', function (e) {
-      e.preventDefault();
-      const formInput = document.querySelector('#searchbar');
-
-      const cityname = formInput.value;
-      const linkapi = `https://api.openweathermap.org/data/2.5/weather?q=${cityname}&appid=66a86bcca603db7ee72a9999499bb3fa&units=metric`;
-
-      fetchWeather(linkapi).then((resp) => {
-        wrapper.innerHTML = '';
-        wrapper.insertAdjacentHTML('beforeend', searchBarComponent());
-        console.log(resp);
-        wrapper.insertAdjacentHTML('beforeend', locationComponent(resp));
-        wrapper.insertAdjacentHTML('beforeend', mainWeatherComponent(resp));
-
-        const mainWeatherEl = document.querySelector('.main-weather-container');
-        mainWeatherEl.insertAdjacentHTML('beforeend', extraInfoComponent(resp));
-      });
-    });
-
-    wrapper.insertAdjacentHTML('beforeend', locationComponent(resp));
-    wrapper.insertAdjacentHTML('beforeend', mainWeatherComponent(resp));
-
-    const mainWeatherEl = document.querySelector('.main-weather-container');
-    mainWeatherEl.insertAdjacentHTML('beforeend', extraInfoComponent(resp));
-  });
+  await renderUI(wrapper, link);
 }
 
-drawUI();
+init();
